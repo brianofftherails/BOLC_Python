d3010409496c0e9e03e348563b40bb8dbab67b6b
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-14 15:56:59 -0400

    Update deliverable.py

--- public/activities/pe1/part5/deliverable.py
+++ public/activities/pe1/part5/deliverable.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import helper as utils
+
 def sentinel():
     return chr(128)
 
@@ -12,7 +14,30 @@ def encode_pgm(msg,coverfilename,outputfilename):
     Returns:
         None
     '''
-    pass
+    try:
+        fobj = open(coverfilename,'r')
+        #fobj.seek(2,0)
+        #file_length = fobj.tell()
+        #msglist = list(msg)
+        #assert (file_length+1) >= (len(msglist)*4)
+        #fobj.seek(0,11) #seek to after header
+        fobj.seek(0,0)
+        cover = fobj.readlines()
+        header = cover[0:4]
+        del cover[0:4]
+        utils.steg_encode(msg,cover)
+        fobj.close()
+    except OSError as e:
+        print(e)
+    try:
+        fobj = open(outputfilename,'w')
+        for line in header:
+            fobj.write("{}\n".format(line))
+        for line in cover:
+            fobj.write("{}\n".format(line))
+        fobj.close()
+    except OSError as e:
+        print(e)
 
 def decode_pgm(filename):
     '''Decodes a message hidden in a PGM file
@@ -21,7 +46,16 @@ def decode_pgm(filename):
     Returns:
         str: the message that was encoded in the PGM file
     '''
-    pass
+    try:
+        fobj = open(filename,'r')
+        decodeme = list()
+        fobj.seek(0,11)
+        for line in fobj.readlines():
+            decodeme.append(line)
+        decoded_list = utils.steg_decode(decodeme)
+        
+    except OSError as e:
+        print(e)
 
 if __name__ == '__main__':
     pass

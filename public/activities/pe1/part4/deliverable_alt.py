b46265c2391e60dbaf38f79b3d5ac6f00242be81
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-14 13:24:07 -0400

    Create deliverable_alt.py

---
+++ public/activities/pe1/part4/deliverable_alt.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def read_pgm(filename):
+    with open(filename,'r') as fp:
+        content = fp.read()
+        content = content.split()
+    return (content[0:4],content[4:])
+
+
+def write_pgm(filename,content):
+    with open(filename,'w') as fp:
+        for line in content[0]+content[1]:
+            fp.write('{}\n'.format(line))
+
+
+def invert_helper(input_string):
+    return str(255-int(input_string))
+  
+
+# This probably won't work with check.py's unittest, but it is a correct solution
+def invert(content):
+    content = (content[0],list(map(invert_helper,content[1])))
+    
+    
+if __name__ == '__main__':
+    pass

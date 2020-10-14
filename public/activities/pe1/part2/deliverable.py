2ed1d0c41c28fba0bbe653150fa8a846474d33d2
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-14 14:53:05 -0400

    Update deliverable.py

--- public/activities/pe1/part2/deliverable.py
+++ public/activities/pe1/part2/deliverable.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
+# Credit to Capt. Matthai
 
 def steg_encode_char(char, cover):
-    '''LSB encodes a character
-    Args:
-        char (str): a single character string
-        cover (list): list of 8 strings representing integers in the range [0-255]
-    Returns:
-        None
-    '''
-    pass
+    #print(cover)
+    msgbin = format(ord(char), "0>8b")
+    #print(msgbin)
+    for i in range(0, 8):
+        coverbinl = list(format(int(cover[i]), "0>8b"))
+        coverbinl[-1] = msgbin[i]
+        cover[i] = str(int("".join(coverbinl), 2))
+        #print(cover[i])
 
 def steg_decode_char(stego):
-    '''LSB decodes a character
-    Args:
-        stego (list): list of 8 strings representing integers in the range [0-255]
-    Returns:
-        str: character that was decoded
-    '''
-    pass
+    msgbits = []
+    for b in stego:
+        msgbits.append(bin(int(b))[-1])
+    message = chr(int("".join(msgbits), 2))
+    return message
 
 if __name__ == '__main__':
-    pass
+    char = 'a'
+    cover = ['250','251','252','251','250','249','248','249']
+    steg_encode_char (char, cover)
+    #steg_decode_char (stego)
+    steg_decode_char(['250','251','253','250','250','248','248','249'])

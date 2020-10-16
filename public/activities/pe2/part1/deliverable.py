20eb5c54d54ada22bf1fe107a162009fcf6ebab0
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-16 16:52:13 -0400

    Update deliverable.py

--- public/activities/pe2/part1/deliverable.py
+++ public/activities/pe2/part1/deliverable.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-from utility import *
+import utility as util
+import helper
+
+def create_mask(bit):
+    return 255+bit
 
 def encode_pgm(msg, infile, outfile):
     '''LSB encodes a message
@@ -11,7 +15,23 @@ def encode_pgm(msg, infile, outfile):
     Returns:
         None
     '''
-    pass
+    fconts = helper.binary_read_pgm(infile)
+    fconts_list = fconts[0]
+    fconts_string = fconts[1]
+    raster_idx = util.raster_index(fconts_string)
+    header = fconts_string[0:raster_idx]
+    cover = fconts_string[raster_idx::]
+    message = bin(int.from_bytes(msg,byteorder='big'))
+    print("Type of message is {} of {}\n".format(type(message),type(message[0])))
+    masked_message = list(map(create_mask,list(int(message))))
+    try:
+        fobj = open(outfile,'w')
+        fobj.write(header)
+        for i in range(raster_idx,len(fconts_list)):
+            fobj.write(fconts_list[i]&masked_message[i])
+        fobj.close()
+    except OSError as e:
+        print(e)
 
 def decode_pgm(infile):
     '''LSB decodes a message

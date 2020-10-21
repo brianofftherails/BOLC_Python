82a9c418bc22ad92aa5d424f1bf7f61085675913
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-21 13:00:44 -0400

    Update exam.py

--- public/activities/practice-exam/v1/exam.py
+++ public/activities/practice-exam/v1/exam.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re as regex
+
 def q1(floatstr):
     '''
     TLO: 112-SCRPY002, LSA 3,4
@@ -7,7 +9,7 @@ def q1(floatstr):
     floats, return a list with each of the floats in the 
     argument as elements in the list.
     '''
-    pass
+    return list(map(float,list(floatstr.split(','))))
 
 def q2(*args):
     '''
@@ -16,7 +18,12 @@ def q2(*args):
     Given the variable length argument list, return the average
     of all the arguments as a float
     '''
-    pass
+    total = 0
+    iteration = 0
+    for arg in args:
+        total += arg
+        iteration += 1
+    return total/iteration
 
 def q3(lst,n):
     '''
@@ -24,7 +31,7 @@ def q3(lst,n):
     Given a list (lst) and a number of items (n), return a new 
     list containing the last n entries in lst.
     '''
-    pass
+    return lst[len(lst)-n::]
     
 def q4(strng):
     '''
@@ -33,7 +40,7 @@ def q4(strng):
     Given an input string, return a list containing the ordinal numbers of 
     each character in the string in the order found in the input string.
     '''
-    pass
+    return [ord(char) for char in strng]
 
 def q5(strng):
     '''
@@ -42,7 +49,7 @@ def q5(strng):
     Given an input string, return a tuple with each element in the tuple
     containing a single word from the input string in order.
     '''
-    pass
+    return tuple(strng.split(' '))
 
 def q6():
     '''
@@ -54,6 +61,10 @@ def q6():
     Date: 01/01/2000 Time: 12:01 a.m. Temperature: 5.2 C
     '''
     pass
+    strng = input("enter strng")
+    match1 = regex.search(r'(\d+/\d+/\d+)',strng)
+    match2 = regex.search(r'(\d+:\d+ .\w\.)',strng)
+    match3 = regex.search(r'/(\d+(\.\d+)?/\s\w)',strng)
 
 def q7(filename):
     '''
@@ -61,7 +72,15 @@ def q7(filename):
     Given a filename, open the file and return the length of the first line 
     in the file excluding the line terminator.
     '''
-    pass
+    try:
+        fobj = open(filename,'r')
+        fobj.seek(0,0) #should be a nop
+        garbage = fobj.readline()
+        length = fobj.tell()-1
+        fobj.close()
+        return length
+    except OSError as e:
+        print(e)
 
 def q8(filename,lst):
     '''
@@ -73,7 +92,15 @@ def q8(filename,lst):
     the list. If "stop" is not found in the list, write the entire list to 
     the file on separate lines.
     '''
-    pass
+    try:
+        fobj = open(filename,'w')
+        for line in lst:
+            if not line == 'stop':
+                fobj.write("{}\n".format(line))
+            else: break
+        fobj.close()
+    except OSError as e:
+        print(e)
 
 def q9(miltime):
     '''
@@ -85,7 +112,15 @@ def q9(miltime):
     1600-2059 "Good Evening"
     2100-0259 "Good Night"
     '''
-    pass
+    if int(miltime) in range(300,1200):
+        return "Good Morning"
+    elif int(miltime) in range(1200,1600):
+        return "Good Afternoon"
+    elif int(miltime) in range(1600,2100):
+        return "Good Evening"
+    elif (int(miltime) in range(2100,2400)) or (int(miltime) in range(0,300)):
+        return "Good Night"
+    else: raise TypeError
     
 def q10(numlist):
     '''
@@ -95,5 +130,8 @@ def q10(numlist):
     numbers in the list are NOT negative. If any numbers in the list are
     negative, return False.
     '''
-    pass
+    for num in numlist:
+        if num < 0:
+            return False
+    return True
 

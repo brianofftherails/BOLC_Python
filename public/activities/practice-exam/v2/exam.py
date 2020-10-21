7002d8fa01b94abaec3fb42eb0468788d58014d2
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2020-10-21 13:33:42 -0400

    Update exam.py

--- public/activities/practice-exam/v2/exam.py
+++ public/activities/practice-exam/v2/exam.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 def q1(sentence):
     '''
     Given a string of multiple words separated by single spaces,
@@ -8,7 +10,7 @@ def q1(sentence):
     'it is accepted as a masterpiece on strategy', the returned
     string should be 'strategy on masterpiece a as accepted is it'.
     '''
-    pass
+    return ' '.join(sentence.split(' ')[::-1])
 
 def q2(n):
     '''
@@ -16,7 +18,14 @@ def q2(n):
     commas seperating groups of 3 digits. For example, given 65535
     the returned string should be '65,535'.
     '''
-    pass
+    revparsed = list(str(n))[::-1]
+    offset = 0
+    for k in range(1,len(revparsed)):
+        if not k%3:
+            revparsed.insert(k+offset,',')
+            offset+=1
+    return ''.join(revparsed[::-1])
+    
 
 def q3(lst0, lst1):
     '''
@@ -25,14 +34,14 @@ def q3(lst0, lst1):
     given [3,4,9] and [8,1,5] the returned list should be [9,8,5,4,3,1].
     The returned list may contain duplicates.
     '''
-    pass
+    return sorted(lst0+lst1,reverse=True)
 
 def q4(s1,s2,s3):
     '''
     Given 3 scores in the range [0-100] inclusive, return 'GO' if
     the average score is greater than 50. Otherwise return 'NOGO'.
     '''
-    pass
+    return 'GO' if sum([s1,s2,s3])/3>50 else 'NOGO'
 
 def q5(integer, limit):
     '''
@@ -41,14 +50,31 @@ def q5(integer, limit):
     limit==30, the returned list should be [0,6,12,18,24,30]. Note, 0 is
     a multiple of any integer except 0 itself.
     '''
-    pass
+    returnedlist = [0]
+    for i in range(integer,limit+1):
+        if not i%integer and not i%2: #not (i%integer or i%2):
+            returnedlist.append(i)
+    return returnedlist
 
 def q6(f0, f1):
     '''
     Given two filenames, return a list whose elements consist of line numbers
     for which the two files differ. The first line is considered line 0.
     '''
-    pass
+    try:
+        fobj0 = open(f0,'r')
+        fobj1 = open(f1,'r')
+        difflist = list()
+        f0conts = fobj0.readlines()
+        f1conts = fobj1.readlines()
+        fobj0.close()
+        fobj1.close()
+        for line in range(0,min(len(f0conts),len(f1conts))):
+            if f0conts[line] != f1conts[line]:
+                difflist.append(line)
+        return difflist
+    except OSError as e:
+        print(e)
 
 def q7(lst):
     '''
@@ -56,14 +82,22 @@ def q7(lst):
     For example, if given [5,7,9,1,3,7,9,5], the returned value should
     be 7.
     '''
-    pass
+    seen_before = list()
+    for val in lst:
+        if val not in seen_before:
+            seen_before.append(val)
+        else: return val
 
 def q8(strng):
     '''
     Given a sentence as a string with words being separated by a single space,
     return the length of the shortest word.
     '''
-    pass
+    smallest = 99999
+    for word in strng.split(' '):
+        if len(word)<smallest:
+            smallest = len(word)
+    return smallest
 
 def q9(strng):
     '''
@@ -73,7 +107,7 @@ def q9(strng):
     'hell9oworld7', the returned character should be 'a' which has
     the ascii value of 97.
     '''
-    pass
+    return chr(int(''.join(re.findall(r'\d',strng))))
 
 def q10(arr):
     '''
@@ -81,4 +115,9 @@ def q10(arr):
     the first non-consecutive value. If all values are consecutive, return
     None. For example, given [1,2,3,4,6,7], the returned value should be 6. 
     '''
-    pass
+    currentval = arr[0]
+    for val in arr:
+        if currentval != val:
+            return val
+        else: currentval += 1
+    return arr[-1]
